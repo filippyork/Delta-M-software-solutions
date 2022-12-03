b5881af7034bb1da3ab0ed47843d1a5f423dbc6d
Filip <filip@Filips-MacBook-Pro.local>
2022-12-03 16:02:51 -0500

    Adding comments for victor on oppendiary.js

--- pages/opendiary.js
+++ pages/opendiary.js
@@ -1,4 +1,4 @@
-var diarytitle = 'Admins Diary'
+var diarytitle = 'Admins Diary' //needs to be changed 
 const username = new URLSearchParams(window.location.search).get('username')
 var url ="http://localhost:3000/post"
 window.onload = function() {
@@ -16,11 +16,13 @@ function response(data, status){
     if(response['action']=='titlereturn'){
         titlelist = response['titlelist']
         console.log(titlelist)
+        //titlelist is the list of titles recieved from the server.
     }
 }
 
 // MADE WITH MULTIPLE DIARIES IN MIND
 function toEditing(){
+    //TODO diary title needs to be defined with the buttonpress as the diary that was chosen
     window.location.href = "./diaryeditting.html?diarytitle="+diarytitle +"&username=" + username
 }
 //TODO for viewing mutliple 

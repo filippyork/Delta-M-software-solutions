8b17ecddd9b15c0a115e833b157aa37083140393
Victor Hoac <77449719+vhoac@users.noreply.github.com>
2022-12-03 17:03:29 -0500

    Updated with server functionality

--- pages/opendiary.js
+++ pages/opendiary.js
@@ -1,6 +1,6 @@
-var diarytitle = 'Admins Diary'
-const username = new URLSearchParams(window.location.search).get('username')
-var url ="http://localhost:3000/post"
+var diarytitle;
+const username = new URLSearchParams(window.location.search).get('username');
+var url = "http://localhost:3000/post";
 window.onload = function() {
     $.post(
         url+'?data='+JSON.stringify({
@@ -10,11 +10,39 @@ window.onload = function() {
     );
 }
 
+function noExistingDiary(diarytitle){
+    $.post(
+        url+"?data="+JSON.stringify({
+            'action' : 'createnewdict',
+            'title' : diarytitle, //TODO This variable needs to be defined (victor) idk if it needs to be changed to a seperate one but currently this is the same as the button title variables
+            'username' : username
+        }),response
+    )
+}
+
 function response(data, status){
-    var response = JSON.parse(data)
-    console.log(data)
-    if(response['action']=='titlereturn'){
-        $("#diaryname").text(response['title'] +" by: "+username)
+    
+    var response = JSON.parse(data);
+    console.log(data);
+
+    if (response['action']=='titlereturn') {
+        
+        //titlelist is the list of titles recieved from the server
+        titlelist = response['titlelist'];
+        console.log(titlelist);
+    }
+    
+    if (response['action']=='createresponse') {
+        
+        if (response['success']) {
+            window.location.href = "./diaryeditting.html?diarytitle=" + diarytitle + "&username=" + username;
+        }
+        
+        else {
+            //TODO (Victor) functionality if diary name already exists (client side)
+            alert("Diary name already exists!") //temporary
+        }
+        
     }
 }
 
@@ -23,19 +51,39 @@ function diaryButtons() {
 
     /* server side will check whether current entry in MyDiary contains 
     the current user's username, an updates the diary title if this is true */
+    
+    /* if no entries exist for current user (e.g. new user), add a new diary option */
+    if (titlelist.length == 0) {
+        var diaryButton = document.createElement('BUTTON');
+        var diarytext = document.createTextNode("Create a New Diary");
+        diaryButton.appendChild(diarytext);
+        
+        // upon clicking this button, prompt user to enter a title
+        diaryButton.onclick = (diarytitle = prompt("Enter a title:"));
 
-    // creates a button to redirect to selected page
-    var diaryButton = document.createElement('BUTTON');
-    var diarytext = document.createTextNode(diarytitle);
-    diaryButton.appendChild(diarytext);
-    diaryButton.id = diarytitle;
+        // send title to function handling empty diary cases
+        noExistingDiary(diarytitle);
+    }
 
-    // when this button is clicked, send user to corresponding page
-    diaryButton.onclick = toEditing(diarytitle);
+    else {
+
+        for (username in titlelist) {
+            // creates a button to redirect to selected page
+            var diaryButton = document.createElement('BUTTON');
+            var diarytext = document.createTextNode(diarytitle);
+            diaryButton.appendChild(diarytext);
+            diaryButton.id = diarytitle;
+        
+            // when this button is clicked, send user to corresponding page
+            diaryButton.onclick = toEditing(diarytitle);    
+
+        }
+
+    }
 
 }
 
 // MADE WITH MULTIPLE DIARIES IN MIND
 function toEditing(diarytitle){
-    window.location.href = "./diaryeditting.html?diarytitle=" + diarytitle;
+    window.location.href = "./diaryeditting.html?diarytitle=" + diarytitle + "&username=" + username;
 }

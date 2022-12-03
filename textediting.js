1f5632c67e95c3f0d595069f127da56ffe99ec93
Lucas <87954647+LucaboiF1@users.noreply.github.com>
2022-12-03 13:20:15 -0500

    Add files via upload

--- textediting.js
+++ textediting.js
@@ -1,3 +1,10 @@
+//all of the diary data
+var totalpagedata = "";
+
+//page count
+var pagecounter = 1;
+
+
 //Load the editable div for the user:
 window.onload = function (){
 
@@ -43,44 +50,70 @@ function sizefunc(value){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 //New page function
 function newpage(){
 
+//store the data as a string
+var editingbox = document.getElementById("editingbox");
+
+//the ^ means end of a page
+totalpagedata = totalpagedata + editingbox.innerHTML + "^" + pagecounter.toString();
 
 
-//add a blank new page to the total amount of pages
+
+//remove text
+editingbox.innerHTML = "";
+
+
+
+     //increent page counter
+     pagecounter = pagecounter + 1
+
+    //create new option in pageselect
+    var pageselect = document.getElementById("page-select");
+    var newoption = document.createElement("option");
+    newoption.setAttribute("value", pagecounter);
+    newoption.innerHTML = pagecounter;
+
+    pageselect.appendChild(newoption);
+
+
+    
+
+//add a blank new page to the total amount of pages. save previous page
 
 
 }
 
-//Next button
-function nextbtn(){
+function pagefunc(value){
+
+var editingbox = document.getElementById("editingbox");    
+
+var v = value - 1;
+
+var prevtemp = totalpagedata.indexOf(v.toString())
+
+var value2 = "^"+value.toString();
+
+var temp = totalpagedata.indexOf(value2.toString());
+
+if(temp == -1){
+
+    editingbox.innerHTML = ""
+    
+} else if (temp != -1) {
+
+    editingbox.innerHTML = ""
+
+for(var i = prevtemp+1; i <= temp-1; i = i + 1){
 
 
-
-//cycle pages right 
+    editingbox.innerHTML = editingbox.innerHTML + totalpagedata[i]
 
 
 
 }
 
-//back button
-function backbtn(){
-
-
-//cycle pages left
-
-
 }
 
+}

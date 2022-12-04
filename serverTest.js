70dd144422bbf4d2d0037f91cf9d7a5dae6ba7c2
Victor Hoac <77449719+vhoac@users.noreply.github.com>
2022-12-04 18:40:05 -0500

    Updated test files
    All test files are prototyped, check functionality of tests

--- serverTest.js
+++ serverTest.js
@@ -24,7 +24,7 @@ describe ('Testing server functionality', function () {
                 }), response
             );
 
-            // TODO: check if user account creation is successful; if not, set flag to false
+            // checks if user account creation is successful; if not, set flag to false
             if (!response) {
                 successUserCreate = false;
             }
@@ -34,102 +34,101 @@ describe ('Testing server functionality', function () {
         assert(successUserCreate);
     })
 
-    // for each user, create one diary and share diary with all other users
-    it('Test 2: For each user, create one diary and share it to all created users', function () {
+    // for first user, create one diary and share diary with all other users
+    it('Test 2: For first user, create one diary and share it to all created users', function () {
 
         var successDiaryCreate = true;
         var successDiaryAddUser = true;
 
-        // login user, create a diary, create a page with content, save diary and share diary to other users
-        for (i = 0; i < 1000 && successDiaryCreate && successDiaryAddUser; i++) {
+        // create one diary with name 'Diary 1'
+        var username = '' + 0 + ''
+        var diarytitle = 'Diary 1'
 
-            // create one diary with name 'Diary 1'
-            var username = '' + i + ''
-            var diarytitle = 'Diary 1'
+        url+"?data="+JSON.stringify({
+            'action' : 'createnewdict',
+            'diarytitle' : diarytitle,
+            'username' : username
+        }),response
+            
+        // create page with some content
+        var content = 'This is first diary'
+        var editingbox = document.getElementById("editingbox")
+        editingbox.innerHTML = content
+        pagefunc(1);
 
-            url+"?data="+JSON.stringify({
-                'action' : 'createnewdict',
-                'diarytitle' : diarytitle,
-                'username' : username
-            }),response
+        // save diary
+        response['action'] = 'confirmdiarysave'
 
-            var content = "This is first diary"
-            pagefunc(1);
-
-            // TODO: create page with some content
-
-            // save diary
-            response['action'] = 'confirmdiarysave'
-
-            // check if diary was successfully created, otherwise set flag to false
-            if (!response) {
-                successDiaryCreate = false;
-            }
-
-
-            // call adduser(), add all other users created in this test
-            for (j = 0; j < 1000 && successDiaryAddUser; j++) {
-
-                // check if user being added is not the same as current user
-                if (i != j) {
-                    $("#addperson").val() = '' + j + ''
-
-                    $.post(url+'?data='+JSON.stringify({
-                        'action' : 'usercheck',
-                        'username' : username,
-                        'shareuser' : $("#addperson").val(),
-                        'diarytitle' : diarytitle
-                    }),response)
-                }
-
-                // TODO: check if user was successfully added
-                if (!response) {
-                    successDiaryAddUser = false;
-                }
-            }
-
-            // logout current user
-            logoutfunc();
-
+        // check if diary was successfully created, otherwise set flag to false
+        if (!response) {
+            successDiaryCreate = false;
         }
+
+
+        // call adduser(), add all other users created in this test
+        for (var j = 1; j < 1000 && successDiaryAddUser; j++) {
+
+            $("#addperson").val() = '' + j + ''
+
+            $.post(url+'?data='+JSON.stringify({
+                    'action' : 'usercheck',
+                    'username' : username,
+                    'shareuser' : $("#addperson").val(),
+                    'diarytitle' : diarytitle
+                }),response)
+
+            // check if user was successfully added
+            if (!response) {
+                successDiaryAddUser = false;
+            }
+        }
+
+        // logout current user
+        logoutfunc();
         
         assert(successDiaryCreate && successDiaryAddUser);
     })
 
-    // check if other users can access the diaries; then check if created diaries have the same content
-    it('Test 3: Check if content of all 1000 created diaries is the same', function () {
+    // check if other users can access the diaries, then check if shared diaries contain the same content as original
+    it('Test 3: Check if content of all shared diaries is the same as the original', function () {
 
         var sameDiaryContent = true;
-        var canAccessDiary = true;
+        var canAccessDiary = false;
+        var i = 1;
 
-        // check if diaries can be accessed by every user
-        for (i = 0; i < 1000 && canAccessDiary; i++) {
+        // check if diary can be accessed by every user
+        do {
 
-            if () {
+            $.post(
+                url+'?data='+JSON.stringify({
+                    'action' : 'titlerequest',
+                    'username' : username
+                }), response
+            )
+
+            // check if current user can access diary, otherwise set flag to false
+            if (response['titlelist'][1] == 'Diary 1') {
 
                 canAccessDiary = false;
             }
-        }
 
+            // retrieve diary content from server
+            $.post(
+                url+'?data='+JSON.stringify({
+                    'action' : 'diaryfetch',
+                    'diarytitle' : diarytitle,
+                    'username' : username
+                }), response
+            )
 
-        // compare content of all created diaries, check if they are the same
-        for (i = 0; i < 1000 && sameDiaryContent; i++) {
+            // compare content of shared diary to that of the original
+            if (document.getElementById("editingbox").innerHTML == 'This is first diary') {
 
-            for (j = 0; j < 1000 && sameDiaryContent; j++) {
-
-                // check if current user being checked is not the same as reference user
-                if (i != j) {
-
-                    // if diary content is different, set flag to false
-                    if () {
-
-                        sameDiaryContent = false;
-                    }
-
-                }
+                sameDiaryContent = false;
             }
-
-        }
+            
+            i++;
+        } while (i < 1000 && canAccessDiary && sameDiaryContent) 
 
         assert(sameDiaryContent && canAccessDiary);
     })

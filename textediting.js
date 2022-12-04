d0cdd91018c34ab7dfa07905decde46d1e6acd29
Lucas <87954647+LucaboiF1@users.noreply.github.com>
2022-12-03 20:31:23 -0500

    Add files via upload

--- textediting.js
+++ textediting.js
@@ -1,4 +1,61 @@
 //all of the diary data
+const varparam = new URLSearchParams(window.location.search)
+var diarytitle = varparam.get('diarytitle')
+var username = varparam.get('username')
+
+console.log(diarytitle, username)
+const url = "http://localhost:3000/post"
+//page count
+var pagecounter = 1;
+
+var totalpagedata
+
+//Load the editable div for the user:
+window.onload = function (){
+
+$('#editingbox').each(function(){
+    this.contentEditable = true;
+});
+
+    $.post(
+        url+'?data='+JSON.stringify({
+            'action' : 'diaryfetch',
+            'diarytitle' : diarytitle,
+            'username' : username
+        }), response
+    )
+
+}
+
+function response(data, status){
+    var response = JSON.parse(data)
+    console.log(data)
+    if(response['action']=='diaryreturn'){
+        totalpagedata = response['diarydata']
+    }
+    if(response['action']=='confirmdiarysave'){
+        console.log("Saved page data redirecting to goodbye page")
+        window.location.href ="./goodbypage.html?diarytitle="+diarytitle +"&username=" + username
+    }
+}
+
+
+
+function saveButton(){
+    $.post(
+        url+'?data='+JSON.stringify({
+            'action' : 'savediary',
+            'diarytitle' : diarytitle,
+            'diarydata' : totalpagedata,
+            'username' : username
+        }), response
+    )
+}
+
+
+
+///////////
+//all of the diary data
 var totalpagedata = [];
 
 //page count
@@ -19,7 +76,7 @@ $('#editingbox').each(function(){
     this.contentEditable = true;
 });
 
-timeout = setTimeout(1000);
+//timeout = setTimeout(1000);
 
 }
 
@@ -73,16 +130,30 @@ function newpage(){
 
 }
 
+var prevselect = 1
 function pagefunc(currentpage){
 
-var editingbox = document.getElementById("editingbox");    
+    var editingbox = document.getElementById("editingbox");
+    totalpagedata[prevselect-1] = editingbox.innerHTML
+    console.log(totalpagedata)
+    editingbox.innerHTML = totalpagedata[currentpage-1]
 
+
+    if(editingbox.innerHTML == "undefined"){
+
+        editingbox.innerHTML = ""
+    }
 
 
 //editingbox.innerHTML = totalpagedata[value+1]
+    prevselect = currentpage
+}
 
 
 
+
+
+function saveandexit(){
 
 
 
